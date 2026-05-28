228e749a8fb6f43b1bc414df045a258d5979b242
Luis Frey <luis@twobrains.com>
2026-05-28 10:42:48 +0200

    feat(Delta): Add CompositeEvent to RemoteReplicator & Mappers

--- src/LionWeb.Core/Core/Notification/Pipe/RemoteReplicator.cs
+++ src/LionWeb.Core/Core/Notification/Pipe/RemoteReplicator.cs
@@ -126,6 +126,9 @@ public class RemoteReplicator : NotificationPipeBase, INotificationHandler
             case ReferenceChangedNotification e:
                 OnRemoteReferenceChanged(e);
                 break;
+            case CompositeNotification e:
+                OnRemoteComposite(correspondingSender, e);
+                break;
             default:
                 throw new ArgumentException($"Can not process notification due to unknown {notification}!");
         }
@@ -424,7 +427,15 @@ public class RemoteReplicator : NotificationPipeBase, INotificationHandler
 
     #endregion
 
-    /// Uses <see cref="IdFilteringNotificationFilter"/> to suppress forwarding notifications raised during executing <paramref name="action"/>. 
+    private void OnRemoteComposite(INotificationSender correspondingSender, CompositeNotification e)
+    {
+        foreach (var item in e.Parts)
+        {
+            Receive(correspondingSender, item);
+        }
+    }
+
+    /// Uses <see cref="IdFilteringNotificationFilter"/> to suppress forwarding notifications raised during executing <paramref name="action"/>.
     protected virtual void SuppressNotificationForwarding(INotification notification, Action action)
     {
         var notificationId = notification.NotificationId;
